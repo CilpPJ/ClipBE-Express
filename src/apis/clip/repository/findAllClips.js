01165ed1dcee79a1af5ff92567bcff31e8a27315
Dobbymin <gmin990624@gmail.com>
2025-09-19 01:16:32 +0900

    feat: supabase 오류 디버깅 코드 적용

--- src/apis/clip/repository/findAllClips.js
+++ src/apis/clip/repository/findAllClips.js
@@ -1,4 +1,5 @@
 import { supabase } from '../../../db/supabase-client.js';
+import { CustomError } from '../../../utils/errors.js';
 
 export const findAllClips = async () => {
   const { data, error } = await supabase.from('clips').select(
@@ -16,7 +17,8 @@ export const findAllClips = async () => {
   );
 
   if (error) {
-    throw error;
+    console.error('Supabase error:', error);
+    throw new CustomError('데이터베이스 조회에 실패했습니다.', 500);
   }
   return data;
 };
