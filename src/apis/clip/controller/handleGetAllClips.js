a040370c6b21ce4e3ce7a7614da6cc0c448fc74a
Dobbymin <gmin990624@gmail.com>
2025-09-19 00:14:21 +0900

    feat: db 관련 코드 분리 및 함수 연결

--- src/apis/clip/controller/handleGetAllClips.js
+++ src/apis/clip/controller/handleGetAllClips.js
@@ -1,4 +1,4 @@
-import { supabase } from '../../../db/supabase-client.js';
+import { getAllClips } from '../service/getAllClips.js';
 
 /**
  * 모든 클립 데이터를 조회하는 API 핸들러
@@ -7,13 +7,9 @@ import { supabase } from '../../../db/supabase-client.js';
  */
 export const handleGetAllClips = async (req, res) => {
   try {
-    const { data, error } = await supabase.from('clips').select('*');
+    const clipsData = await getAllClips();
 
-    if (error) {
-      throw error;
-    }
-
-    res.status(200).json(data);
+    res.status(200).json(clipsData);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
